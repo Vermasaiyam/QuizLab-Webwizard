9fe74aad8b7eb6698c65ba253fa6b446735a5659
Vermasaiyam <vermasaiyam9@gmail.com>
2025-02-04 11:43:09 +0530

    bug fixed

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { Loader2 } from "lucide-react";
